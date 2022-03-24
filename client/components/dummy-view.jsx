d6b8b764ae320d298f3476f20d7fcb6741679ffb
Ivan <xpynkostb@gmail.com>
2022-03-24 21:17:30 +0300

    initial beautify

--- client/components/dummy-view.jsx
+++ client/components/dummy-view.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Head from './head'
 
 const Dummy = () => {
-  const { planetId } = useParams()
   return (
     <div>
       <Head title="Dummy" />
       <div className="flex justify-center items-center h-screen">
         <div className="flex flex-col justify-center bg-neutral-900 p-10 rounded-xl select-none">
-          <span className="text-white text-right font-semibold">Boilerplate {planetId}</span>
+          <span className="text-white text-center font-semibold">Boilerplate</span>
           <div>
             <Link to="/dashboard" style={{ color: 'white' }}>
               Go To Dashboard
--- client/components/home.jsx
+++ client/components/home.jsx
@@ -1,19 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import Head from './head'
-// import wave from '../assets/images/wave.jpg'
 
 const Home = () => {
-  const [counter, setCounterNew] = useState(0)
-
   return (
     <div>
       <Head title="Dashboard" />
-      <button type="button" onClick={() => setCounterNew(counter + 1)}>
-        updateCounter
-      </button>
-      <div> Hello World Dashboard {counter} </div>
-      <Link to="/">Go To Dummy</Link>
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col justify-center bg-neutral-900 p-10 rounded-xl select-none">
+          <span className="text-white text-center font-semibold">Dashboard</span>
+          <div>
+            <Link to="/" style={{ color: 'white' }}>
+              Go To Dummy
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
