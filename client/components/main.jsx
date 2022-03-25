86c58fca9ccf9abd304e1f1e873c713be7d043ad
Ivan <xpynkostb@gmail.com>
2022-03-25 14:53:17 +0300

    main page done

--- client/components/main.jsx
+++ client/components/main.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
-// import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
 import Head from './head'
 
 const Main = () => {
+  const [userName, setUserName] = useState('')
+  const InputeChange = (e) => {
+    setUserName(e.target.value)
+  }
+  const url = `/${userName}`
+
   return (
     <div>
       <Head title="Main" />
       <div className="flex justify-center items-center h-screen">
-        <div className="flex flex-col justify-center bg-neutral-900 p-10 rounded-xl select-none">
-          <span className="text-white text-center font-semibold">Main</span>
-          {/* <div>
-            <Link to="/dashboard" style={{ color: 'white' }}>
-              Go To Dashboard
-            </Link>
-          </div> */}
+        <div className="bg-neutral-900 p-10 rounded-xl select-none text-white text-center font-semibold">
+          <div className="p-10">This is Main</div>
+          <div className="">
+            <span>Enter user name: </span>
+            <input
+              type="text"
+              id="input-field"
+              value={userName}
+              onChange={InputeChange}
+              className="text-black m-1 p-1"
+            />
+            <a href={url} className="p-0 m-1">
+              <button
+                type="button"
+                id="search-button"
+                className="m-7 m-0 p-1 box-border bg-red-500"
+              >
+                Search
+              </button>
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- client/components/profile.jsx
+++ client/components/profile.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import Head from './head'
 
 const Profile = () => {
+  const { userName } = useParams()
   return (
     <div>
       <Head title="Profile" />
       <div className="flex justify-center items-center h-screen">
         <div className="flex flex-col justify-center bg-neutral-900 p-10 rounded-xl select-none">
-          <span className="text-white text-center font-semibold">Profile</span>
+          <span className="text-white text-center font-semibold">This is {userName} profile</span>
           <div>
             <Link to="/" style={{ color: 'white' }}>
               Go To Main
