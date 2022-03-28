1993b542198b6a589c7529ef3253f05f1ef0214d
Ivan <xpynkostb@gmail.com>
2022-03-28 15:40:53 +0300

    set redux for synchronized functions

--- client/components/main.jsx
+++ client/components/main.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 import Head from './head'
 
+import { changeName } from '../redux/reducers/user'
+
 const Main = () => {
+  const dispatch = useDispatch()
   const userName = useSelector((s) => s.user.name)
   return (
     <div>
       <Head title="Main" />
-      <div className="flex justify-center items-center h-screen">
-        <p className="font-bold text-xl">Hello world {userName}</p>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <p className="font-bold text-xl">Hello {userName}</p>
+        <div>
+          <input
+            type="text"
+            className="border-4 border-yellow-500"
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                dispatch(changeName(e.target.value))
+              }
+            }}
+          />
+        </div>
       </div>
     </div>
   )
--- client/redux/reducers/user.js
+++ client/redux/reducers/user.js
@@ -1,10 +1,22 @@
+const CHANGE_NAME = 'CHANGE_NAME'
+
 const initialState = {
   name: 'Fin'
 }
 
 export default (state = initialState, action = {}) => {
   switch (action.type) {
+    case CHANGE_NAME: {
+      return {
+        ...state,
+        name: action.newName
+      }
+    }
     default:
       return state
   }
+}
+
+export function changeName(inputName) {
+  return { type: CHANGE_NAME, newName: inputName }
 }
