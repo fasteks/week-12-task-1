c6e272cc58561810e34833b087d483c6a6ab693f
Ivan <xpynkostb@gmail.com>
2022-03-29 20:13:48 +0300

    formated cards to unified template

--- client/components/card.jsx
+++ client/components/card.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Card = (props) => {
   const { card } = props
   return (
-    <div className="card flex flex-col items-center p-2 m-2 bg-lime-100 border-2 rounded-lg border-lime-600 ">
+    <div className="card flex flex-col items-center justify-between p-2 m-2 bg-lime-100 border-2 rounded-lg border-lime-600 w-1/6 h-96">
       <img className="card__image" src={card.image} alt={card.description} />
-      <p className="card__title font-bold">{card.title}</p>
+      <p className="card__title text-center font-bold">{card.title}</p>
       <p className="card__price">Price: {card.price}</p>
       <span className="currency">Currency: 2</span>
       {true && <p className="card__product--amount">Quantity in Basket: 4</p>}
