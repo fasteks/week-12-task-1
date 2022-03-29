dd366df2b3a309b26ec79065520955069acb0ed0
Ivan <xpynkostb@gmail.com>
2022-03-29 12:36:53 +0300

    clean cards view

--- client/components/card.jsx
+++ client/components/card.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Card = (props) => {
   const { card } = props
   return (
-    <div className="card">
+    <div className="card flex flex-col items-center p-2 m-2 bg-lime-100 border-2 rounded-lg border-lime-600 ">
       <img className="card__image" src={card.image} alt={card.description} />
-      <p className="card__price">{card.price}</p>
-      <span className="currency">2</span>
-      <p className="card__title">{card.title}</p>
-      <p className="card__product--amount">4</p>
+      <p className="card__title font-bold">{card.title}</p>
+      <p className="card__price">Price: {card.price}</p>
+      <span className="currency">Currency: 2</span>
+      {true && <p className="card__product--amount">Quantity in Cart: 4</p>}
       <button type="button">Add to Cart</button>
     </div>
   )
