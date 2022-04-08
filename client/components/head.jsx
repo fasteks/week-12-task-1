c30ba30273ed8787f032d645741355d6faf30408
Ivan <xpynkostb@gmail.com>
2022-04-08 14:58:38 +0300

    fixing favicon

--- client/components/head.jsx
+++ client/components/head.jsx
@@ -10,7 +10,7 @@ const Head = (props) => (
     <meta charSet="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#FF0000" />
-    <link rel="icon" href="data:;base64,=" />
+    <link rel="icon" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAADElEQVQI12P4//8/AAX+Av7czFnnAAAAAElFTkSuQmCC" />
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
